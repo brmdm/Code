3eda24a83678418045507339166d6ce22650efa7
brmdm <N12c345hjkokiaf>
2020-07-13 22:24:22 +0300

    Synchronization

---
+++ src/rd/java/basic/practice3/Demo.java
@@ -0,0 +1,9 @@
+package rd.java.basic.practice3;
+
+public class Demo {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/rd/java/basic/practice3/Part3.java
@@ -0,0 +1,4 @@
+package rd.java.basic.practice3;
+
+public class Part3 {
+}
