f317c21ac03c494ca2956a7e26b08f7104c526f0
brmdm <N12c345hjkokiaf>
2020-07-14 23:27:35 +0300

    Synchronization

--- src/rd/java/basic/practice3/Part4.java
+++ src/rd/java/basic/practice3/Part4.java
@@ -1,12 +1,26 @@
 package rd.java.basic.practice3;
 
+import java.security.*;
+
 public class Part4 {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws NoSuchAlgorithmException  {
+        System.out.println(hash("asdf", "MD5"));
+        System.out.println(hash("asdf", "SHA-256"));
+        System.out.println(hash("asdf", "SHA-512"));
     }
 
-    public static String hash(String input, String algorithm) {
-        return null;
+    public static String hash(String input, String algorithm) throws NoSuchAlgorithmException  {
+        StringBuffer hexString = new StringBuffer();
+        MessageDigest digest = MessageDigest.getInstance(algorithm);
+        digest.update(input.getBytes());
+        byte[] dig = digest.digest();
+
+        for (int i = 0; i < dig.length; i++) {
+            String hex = Integer.toHexString(0xff & dig[i]);
+            if(hex.length() == 1) hexString.append('0');
+            hexString.append(hex);
+        }
+        return hexString.toString().toUpperCase();
     }
 }
