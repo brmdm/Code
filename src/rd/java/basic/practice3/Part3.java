620f53d359f7bfdb23cd2e635721617c0a2aeeb9
brmdm <N12c345hjkokiaf>
2020-07-14 22:44:37 +0300

    Synchronization

--- src/rd/java/basic/practice3/Part3.java
+++ src/rd/java/basic/practice3/Part3.java
@@ -1,12 +1,52 @@
 package rd.java.basic.practice3;
 
+import java.util.Scanner;
+
 public class Part3 {
 
     public static void main(String[] args) {
-
+        String input = Util.getInput("part3.txt");
+        System.out.print(convert(input));
     }
 
     public static String convert(String input) {
-        return null;
+        StringBuilder sb = new StringBuilder();
+        StringBuilder sbchar = new StringBuilder();
+        Scanner read = new Scanner(input);
+        read.useDelimiter("\\s");
+        String[] str1 = new String[100];
+        char[] word = new char[20];
+
+        for (int i = 0; read.hasNext(); i++) {
+            str1[i] = read.next();
+        }
+
+        for (int i = 0; str1[i] != null ; i++) {
+            word = str1[i].toCharArray();
+            if (word.length >= 3) {
+                if (Character.isLowerCase(word[0])) {
+                    word[0] = Character.toUpperCase(word[0]);
+                    for (int j = 0; j < word.length ; j++) {
+                        sbchar.append(word[j]);
+                    }
+                    str1[i] = sbchar.toString();
+                    sbchar.delete(0, sbchar.length());
+                } else {
+                    word[0] = Character.toLowerCase(word[0]);
+                    for (int j = 0; j < word.length ; j++) {
+                        sbchar.append(word[j]);
+                    }
+                    str1[i] = sbchar.toString();
+                    sbchar.delete(0, sbchar.length());
+                }
+            }
+        }
+
+        for (int i = 0; str1[i] != null ; i++) {
+            sb.append(str1[i] + " ");
+        }
+
+        sb.deleteCharAt(sb.length() - 1);
+        return sb.toString();
     }
 }
