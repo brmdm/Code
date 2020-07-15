f8bff3c7e5dd6ca3da50e9ec58d57a53ba18593b
brmdm <N12c345hjkokiaf>
2020-07-15 11:53:49 +0300

    Synchronization

--- src/rd/java/basic/practice3/Demo.java
+++ src/rd/java/basic/practice3/Demo.java
@@ -1,9 +1,47 @@
 package rd.java.basic.practice3;
 
+
+import java.security.NoSuchAlgorithmException;
+
 public class Demo {
     @SuppressWarnings("all")
     public static void main(String[] args) {
+        //Part1
+        String input = Util.getInput("part1.txt");
+        System.out.print(Part1.convert1(input));
+        System.out.println();
+        System.out.print(Part1.convert2(input));
+        System.out.println();
+        System.out.print(Part1.convert3(input));
+        System.out.println();
+        System.out.print(Part1.convert4(input));
+        //Part2
+        String input2 = Util.getInput("part2.txt");
+        System.out.println(Part2.convert(input2));
+        //Part3
+        String input3 = Util.getInput("part3.txt");
+        System.out.println(Part3.convert(input3));
+        //Part4
+        try {
+            System.out.println(Part4.hash("asdf", "SHA-512"));
+            System.out.println(Part4.hash("asdf", "MD5"));
+            System.out.println(Part4.hash("asdf", "SHA-256"));
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        //Part5
+        for (int i = 1; i <= 100; i++) {
+            System.out.print(i + Part5.ARROW + Part5.decimal2Roman(i) + Part5.ARROW +
+                    Part5.roman2Decimal(Part5.decimal2Roman(i)));
 
+            if (i < 100) {
+                System.out.print("\n");
+            }
+        }
+        System.out.println();
+        //Part6
+        String input6 = Util.getInput("part6.txt");
+        System.out.print(Part6.convert(input6));
     }
 
 }
--- src/rd/java/basic/practice3/Part5.java
+++ src/rd/java/basic/practice3/Part5.java
@@ -1,16 +1,77 @@
 package rd.java.basic.practice3;
 
 public class Part5 {
+    public static final String ARROW = " --> ";
+
     @SuppressWarnings("all")
     public static void main(String[] args) {
-
+        for (int i = 1; i <= 100; i++) {
+            System.out.print(i + ARROW + decimal2Roman(i) + ARROW + roman2Decimal(decimal2Roman(i)));
+            if (i < 100) {
+                System.out.print("\n");
+            }
+        }
     }
 
     public static String decimal2Roman(int dec) {
-        return null;
+        String[] romanCharacters = {"M", "CM", "D", "C", "XC", "L", "X", "IX", "V", "I"};
+        int[] romanValues = {1000, 900, 500, 100, 90, 50, 10, 9, 5, 1};
+        StringBuilder sb = new StringBuilder("");
+        String result = "";
+        for (int i = 0; i < romanValues.length; i++) {
+            int numberInPlace = dec / romanValues[i];
+            if (numberInPlace == 0) continue;
+            result = numberInPlace == 4 && i > 0 ? romanCharacters[i] + romanCharacters[i - 1] :
+                    new String(new char[numberInPlace]).replace("\0", romanCharacters[i]);
+            sb.append(result);
+            dec = dec % romanValues[i];
+        }
+        return sb.toString();
     }
 
     public static int roman2Decimal(String roman) {
-        return 0;
+        int decimalNum = 0;
+        int l = roman.length();
+        int num = 0;
+        int previousnum = 0;
+        for (int i = l - 1; i >= 0; i--) {
+            char x = roman.charAt(i);
+            x = Character.toUpperCase(x);
+            switch (x) {
+                case 'I':
+                    previousnum = num;
+                    num = 1;
+                    break;
+                case 'V':
+                    previousnum = num;
+                    num = 5;
+                    break;
+                case 'X':
+                    previousnum = num;
+                    num = 10;
+                    break;
+                case 'L':
+                    previousnum = num;
+                    num = 50;
+                    break;
+                case 'C':
+                    previousnum = num;
+                    num = 100;
+                    break;
+                case 'D':
+                    previousnum = num;
+                    num = 500;
+                    break;
+                case 'M':
+                    previousnum = num;
+                    num = 1000;
+                    break;
+            }
+            if (num < previousnum) {
+                decimalNum = decimalNum - num;
+            } else
+                decimalNum = decimalNum + num;
+        }
+        return decimalNum;
     }
 }
