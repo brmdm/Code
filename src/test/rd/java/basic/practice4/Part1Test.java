370218b526463baa7b6270e25959307efe02cfe6
brmdm <N12c345hjkokiaf>
2020-07-26 23:19:55 +0300

    Synchronization

--- src/test/rd/java/basic/practice4/Part1Test.java
+++ src/test/rd/java/basic/practice4/Part1Test.java
@@ -1,24 +1,27 @@
 package test.rd.java.basic.practice4;
 
 import org.junit.Test;
-import rd.java.basic.practice3.Util;
-
 import static junit.framework.Assert.assertEquals;
-import static rd.java.basic.practice3.Part1.*;
+import rd.java.basic.practice4.Part1;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 public class Part1Test {
     @Test
     public void main() {
-        String input = Util.getInput("part1.txt");
-        assertEquals("ivanov: ivanov@mail.com\n" +
-                "РїРµС‚СЂРѕРІ: petrov@google.com\n" +
-                "obama: obama@google.com\n" +
-                "bush: bush@mail.com", convert1(input));
-        assertEquals("Ivanov Ivan (email:ivanov@mail.com)\n" +
-                "РџРµС‚СЂРѕРІ РџРµС‚СЂ (email:petrov@google.com)\n" +
-                "Obama Barack (email:obama@google.com)\n" +
-                "Р‘СѓС€ Р”Р¶РѕСЂРґР¶ (email:bush@mail.com)", convert2(input));
-        assertEquals("mail.com ==> ivanov, bush\r\n" +
-                "google.com ==> РїРµС‚СЂРѕРІ, obama", convert3(input));
+        PrintStream originalOut = System.out; // to have a way to undo the binding with your `ByteArrayOutputStream`
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(bos));
+        // action
+        Part1.main(null);
+        // assertion
+        assertEquals("en I was unger\n" +
+                "So ch unger\n" +
+                "an day\n" +
+                "формацию из йла\n" +
+                "в рхний гистр\n", bos.toString());
+        // undo the binding in System
+        System.setOut(originalOut);
     }
 }
