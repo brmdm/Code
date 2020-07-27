7f3cbd72279f176b5b064d1d3b02f1f5ccba339f
brmdm <N12c345hjkokiaf>
2020-07-27 10:32:12 +0300

    Synchronization

--- src/test/rd/java/basic/practice4/Part4Test.java
+++ src/test/rd/java/basic/practice4/Part4Test.java
@@ -1,19 +1,26 @@
 package test.rd.java.basic.practice4;
 
 import org.junit.Test;
-
-import java.security.NoSuchAlgorithmException;
-
 import static junit.framework.Assert.assertEquals;
-import static rd.java.basic.practice3.Part4.hash;
+import rd.java.basic.practice4.Part4;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 public class Part4Test {
     @Test
-    public void main() throws NoSuchAlgorithmException {
-        assertEquals("912EC803B2CE49E4A541068D495AB570", hash("asdf", "MD5"));
-        assertEquals("F0E4C2F76C58916EC258F246851BEA091D14D4247A2FC3E18694461B1816E13B",
-                hash("asdf", "SHA-256"));
-        assertEquals("401B09EAB3C013D4CA54922BB802BEC8FD5318192B0A75F201D8B3727429080FB337591ABD3E44453B954555B7A0812E1081C39B740293F765EAE731F5A65ED1",
-                hash("asdf", "SHA-512"));
+    public void main() {
+        PrintStream originalOut = System.out;
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(bos));
+
+        Part4.main(null);
+
+        assertEquals(" ласс должен разбирать текстовый файл и возвращать предложени€ из файла.\n" +
+                "ѕод предложением понимать последовательность, котора€ начинаетс€ с большой буквы и заканчиваетс€ точкой.\n" +
+                "»сходный файл брать небольшим по размеру.\n" +
+                "ƒостаточно несколько строк и несколько предложений.\n", bos.toString());
+        // undo the binding in System
+        System.setOut(originalOut);
     }
 }
