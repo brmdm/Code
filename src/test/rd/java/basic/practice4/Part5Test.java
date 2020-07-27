4333852bd45baa8266143ededbcec67a9288d308
brmdm <N12c345hjkokiaf>
2020-07-27 09:53:49 +0300

    Synchronization

--- src/test/rd/java/basic/practice4/Part5Test.java
+++ src/test/rd/java/basic/practice4/Part5Test.java
@@ -1,19 +1,28 @@
 package test.rd.java.basic.practice4;
 
 import org.junit.Test;
-
 import static junit.framework.Assert.assertEquals;
-import static rd.java.basic.practice3.Part5.decimal2Roman;
-import static rd.java.basic.practice3.Part5.roman2Decimal;
+
+import rd.java.basic.practice4.Part5;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 
 public class Part5Test {
     @Test
     public void main() {
-        assertEquals("XCVIII", decimal2Roman(98));
-        assertEquals("XCIX", decimal2Roman(99));
-        assertEquals("XXX", decimal2Roman(30));
-        assertEquals(98, roman2Decimal("XCVIII"));
-        assertEquals(99, roman2Decimal("XCIX"));
-        assertEquals(30, roman2Decimal("XXX"));
+        PrintStream originalOut = System.out; // to have a way to undo the binding with your `ByteArrayOutputStream`
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(bos));
+
+        System.setIn(new ByteArrayInputStream(
+                "apple en^table en^apple en^stop".replace("^", System.lineSeparator()).getBytes(StandardCharsets.UTF_8)));
+        Part5.main(null);
+        assertEquals("apple\r\n" +
+                "table\r\n" +
+                "apple\r\n", bos.toString());
+        // undo the binding in System
+        System.setOut(originalOut);
     }
 }
