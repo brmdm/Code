7c1b4e4bee38ce392efd51b87b76f3160b8934e6
brmdm <N12c345hjkokiaf>
2020-07-27 10:01:51 +0300

    Synchronization

--- src/test/rd/java/basic/practice4/Part6Test.java
+++ src/test/rd/java/basic/practice4/Part6Test.java
@@ -1,20 +1,27 @@
 package test.rd.java.basic.practice4;
 
 import org.junit.Test;
-import rd.java.basic.practice3.Util;
-
 import static junit.framework.Assert.assertEquals;
-import static rd.java.basic.practice3.Part6.convert;
+import rd.java.basic.practice4.Part6;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 
 public class Part6Test {
     @Test
     public void main() {
-        String input = Util.getInput("part6.txt");
-        assertEquals("This _is _a _test\r\n" +
-                "_And this _is _also _a _test\r\n" +
-                "_And these are _also tests\r\n" +
-                "_test _Р­С‚Рѕ _С‚РµСЃС‚\r\n" +
-                "_Р­С‚Рѕ _С‚Р°РєР¶Рµ _С‚РµСЃС‚\r\n" +
-                "Р СЌС‚Рѕ _С‚Р°РєР¶Рµ С‚РµСЃС‚С‹", convert(input));
+        PrintStream originalOut = System.out;
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(bos));
+        System.setIn(new ByteArrayInputStream(
+                "Latn^Cyrl^asdf^latn^cyrl^stop".replace("^", System.lineSeparator()).getBytes(StandardCharsets.UTF_8)));
+        Part6.main(null);
+        assertEquals("Latn: Is there anybody going to listen to my story \r\n" +
+                "Cyrl: Лягає день Він віддає свої надії ночі Робітники Заморились працювати С барабаном ходит ёжик Бум бум бум \r\n" +
+                "asdf: Incorrect input\r\n" +
+                "latn: Incorrect input\r\n" +
+                "cyrl: Incorrect input\r\n", bos.toString());
+        System.setOut(originalOut);
     }
 }
