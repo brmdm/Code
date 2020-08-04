05aff1aafbde02ae550cb0f141258f41ed1f44f7
brmdm <N12c345hjkokiaf>
2020-08-04 12:57:13 +0300

    Synchronization

--- src/rd/java/basic/practice5/Part3.java
+++ src/rd/java/basic/practice5/Part3.java
@@ -1,21 +1,77 @@
 package rd.java.basic.practice5;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class Part3 {
-
     private int counter;
-
     private int counter2;
+    private final int threads;
+    private final int iterations;
+    private final Thread[] t;
 
-    public static void main(final String[] args) {
-
+    public Part3(int numberOfThreads, int numberOfIterations) {
+        this.threads = numberOfThreads;
+        this.iterations = numberOfIterations;
+        this.t = new Thread[threads];
     }
 
-    public void compare() {
-
+    public static void main(final String[] args) throws InterruptedException {
+        Part3 p3 = new Part3(10, 5);
+        p3.compare();
+        System.out.println("-----------------------------------");
+        p3.counter = 0;
+        p3.counter2 = 0;
+        p3.compareSync();
     }
 
-    public void compareSync() {
+    public void compare() throws InterruptedException {
+        ExecutorService es = Executors.newCachedThreadPool();
+        for (int i = 0; i < t.length; i++) {
+            t[i] = new Thread(() -> {
+                int count = 0;
+                while (count < iterations) {
+                    System.out.println(counter == counter2);
+                    counter++;
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    counter2++;
+                    count++;
+                }
+            });
+            es.execute(t[i]);
+        }
+        es.shutdown();
+        while(!es.awaitTermination(1, TimeUnit.MINUTES));
+    }
 
+    public void compareSync() throws InterruptedException {
+        ExecutorService es = Executors.newCachedThreadPool();
+        for (int i = 0; i < t.length; i++) {
+            t[i] = new Thread(() -> {
+                synchronized (this) {
+                    int count = 0;
+                    while (count < iterations) {
+                        System.out.println(counter == counter2);
+                        counter++;
+                        try {
+                            Thread.sleep(100);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        counter2++;
+                        count++;
+                    }
+                }
+            });
+            es.execute(t[i]);
+        }
+        es.shutdown();
+        while(!es.awaitTermination(1, TimeUnit.MINUTES));
     }
 
 }
