15225ce20f13f51a844d9c2be82990d3c8399049
brmdm <N12c345hjkokiaf>
2020-08-04 18:59:35 +0300

    Synchronization

--- src/rd/java/basic/practice5/Part4.java
+++ src/rd/java/basic/practice5/Part4.java
@@ -3,17 +3,22 @@ package rd.java.basic.practice5;
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Part4 {
-        private static String[] array1 = new String[20];
+    private static String[] array1 = new String[20];
+    private static Thread[] t = new Thread[10];
+    private static int globalMax = 0;
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         String input = getInput("part4.txt");
         Scanner scanner = new Scanner(input);
-        scanner.useDelimiter("\\n");
+        scanner.useDelimiter("\\r\\n");
         try {
             int arrayCounter = 0;
             while (scanner.hasNext()) {
@@ -24,64 +29,55 @@ public class Part4 {
             scanner.close();
         }
 
-        searchMax(array1);
+        System.out.println(multiSearch(array1));
+        System.out.print(searchMax(array1));
+
     }
 
-//    private static String multiSearch (String[] input) {
-//        int max = 0;
-//        StringBuilder sb = new StringBuilder();
-//        Logger logger = Logger.getLogger(Part4.class.getName());
-//        int[] digits = new int[150];
-//        for (int i = 0; input[i] != null; i++) {
-//            String [] strarray = input[i].split(" ");
-//            for (int j = 0; j < strarray.length; j++) {
-//
-//            }
-//        }
-//        for (int i = 0; input[i] != null; i++) {
-//            new Thread(() -> {
-//                    int count = 0;
-//                    while () {
-//                        try {
-//                            Thread.sleep(1);
-//                        } catch (InterruptedException e) {
-//                            String message = "Exception in not sync Comparing";
-//                            logger.log(Level.ALL, message, e);
-//                        }
-//
-//                    }
-//            });
-//        }
-//
-//        return sb.toString();
-//    }
-
-    private static String searchMax (String[] input) {
-        int max = 0;
+    private static String multiSearch(String[] input) throws InterruptedException {
+        long start = System.nanoTime();
+        ExecutorService es = Executors.newCachedThreadPool();
         StringBuilder sb = new StringBuilder();
 
-        long start = System.nanoTime();
         for (int i = 0; input[i] != null; i++) {
-            String [] strarray = input[i].split(" ");
-            for (int j = 0; j < strarray.length; j++) {
-                Scanner scanner = new Scanner(strarray[j]);
-                if (scanner.hasNextInt()) {
+            int finalI = i;
+            t[i] = new Thread(() -> {
+                String[] strarray = input[finalI].split(" ");
+                for (int j = 0; j < strarray.length; j++) {
                     int value = Integer.parseInt(strarray[j]);
-                    if (value > max) {
-                        max = value;
+                    if (value > globalMax) {
+                        globalMax = value;
                     }
                 }
 
-            }
+            });
+            es.execute(t[i]);
         }
 
+        es.shutdown();
+        while(!es.awaitTermination(1, TimeUnit.MINUTES));
+        long time = (System.nanoTime() - start) / 1_000_000;
+        sb.append(globalMax + "\n" + time);
+        return sb.toString();
+    }
 
+    private static String searchMax(String[] input) {
+        long start = System.nanoTime();
+        int max = 0;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; input[i] != null; i++) {
+            String[] strarray = input[i].split(" ");
+            for (int j = 0; j < strarray.length; j++) {
+                int value = Integer.parseInt(strarray[j]);
+                if (value > max) {
+                    max = value;
+                }
+            }
+        }
         long time = (System.nanoTime() - start) / 1000;
         sb.append(max + "\n" + time);
         return sb.toString();
     }
-
-
 
 
     private static String getInput(String fileName) {
