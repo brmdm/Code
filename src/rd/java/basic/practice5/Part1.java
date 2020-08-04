9676451ee9201802aadbdd8922da31970dbc98b7
brmdm <N12c345hjkokiaf>
2020-08-04 10:31:13 +0300

    Synchronization

--- src/rd/java/basic/practice5/Part1.java
+++ src/rd/java/basic/practice5/Part1.java
@@ -1,7 +1,47 @@
 package rd.java.basic.practice5;
 
 public class Part1 {
-    public static void main(String[] args) {
+    static Thread t1 = new Thread(new NameThreadImpl());
+    static Thread t2 = new NameThreadExt();
 
+    public static void main(String[] args) throws InterruptedException {
+        t1.start();
+        Thread.sleep(2000);
+        t2.start();
+        Thread.sleep(2000);
     }
+
+    static class NameThreadImpl implements Runnable {
+        int count = 0;
+        @Override
+        public void run() {
+            while (count < 4) {
+                System.out.println(t1.getName());
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                count++;
+            }
+        }
+    }
+
+    static class NameThreadExt extends Thread {
+        int count = 0;
+        @Override
+        public void run() {
+            while (count < 4) {
+                System.out.println(t2.getName());
+                try {
+                    sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                count++;
+            }
+        }
+    }
+
+
 }
