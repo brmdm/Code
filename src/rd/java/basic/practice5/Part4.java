1f52dad8cee2a5982d90690d33bb8f2a263e5c7a
brmdm <N12c345hjkokiaf>
2020-08-04 13:11:18 +0300

    Synchronization

--- src/rd/java/basic/practice5/Part4.java
+++ src/rd/java/basic/practice5/Part4.java
@@ -1,7 +1,50 @@
 package rd.java.basic.practice5;
 
-public class Part4 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+public class Part4 {
+
+
+
+    public static void main(String[] args) {
+        String input = getInput("part4.txt");
+        String[] array1 = new String[20];
+        int arrayCounter = 0;
+        Scanner scanner = new Scanner(input);
+        scanner.useDelimiter("\\s|\\n");
+
+        try {
+            while (scanner.hasNext()) {
+                array1[arrayCounter] = scanner.next();
+            }
+        } finally {
+            scanner.close();
+        }
+
+
+    }
+
+
+
+
+    private static String getInput(String fileName) {
+        Logger logger = Logger.getLogger(Part4.class.getName());
+        StringBuilder sb = new StringBuilder();
+        try {
+            Scanner scanner = new Scanner(new File(fileName), "cp1251");
+            while (scanner.hasNextLine()) {
+                sb.append(scanner.nextLine()).append(System.lineSeparator());
+            }
+            scanner.close();
+            return sb.toString().trim();
+        } catch (IOException ex) {
+            String message = "Unexpected Input Exception";
+            logger.log(Level.ALL, message, ex);
+        }
+        return sb.toString();
     }
 }
