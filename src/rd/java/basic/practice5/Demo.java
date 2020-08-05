cb30750813fd78e5964b5670dd2ae5330c263842
brmdm <N12c345hjkokiaf>
2020-08-05 12:24:17 +0300

    Synchronization

--- src/rd/java/basic/practice5/Demo.java
+++ src/rd/java/basic/practice5/Demo.java
@@ -1,10 +1,13 @@
 package rd.java.basic.practice5;
 
+import java.io.IOException;
+
 public class Demo {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException {
         Part1.main(null);
         Part2.main(null);
         Part3.main(null);
         Part4.main(null);
+        Part5.main(null);
     }
 }
