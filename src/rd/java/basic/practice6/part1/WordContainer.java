febb068ee91d53d3321936b74742c2f514a5174d
brmdm <N12c345hjkokiaf>
2020-08-19 13:14:02 +0300

    Synchronization

--- src/rd/java/basic/practice6/part1/WordContainer.java
+++ src/rd/java/basic/practice6/part1/WordContainer.java
@@ -1,19 +1,18 @@
 package rd.java.basic.practice6.part1;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class WordContainer {
-    private Word [] words = new Word[100];
+    private Word[] words = new Word[100];
     private int counter = 0;
 
 
     public static void main(String[] args) {
-        WordContainer wordContainer= new WordContainer();
+        WordContainer wordContainer = new WordContainer();
         boolean repeat = true;
 
         Scanner console = new Scanner(System.in);
         console.useDelimiter("\\s|\\n|\\r\\n");
-
 
 
         while (repeat) {
@@ -30,31 +29,48 @@ public class WordContainer {
         }
     }
 
-    private void compare (String o) {
+    private void compare(String o) {
+        boolean equalsFlag = true;
         boolean equals;
-        for (int i = 0; words[i] != null; i++) {
-//            if (i == 0) {
-//                words[counter] = new Word(o);
-//                break;
-//            }
-            equals = words[i].getContent().equals(o);
-            if (equals) {
-                words[i].increaseFrequency();
-            } else {
-                words[counter] = new Word(o);
+        String[] variableWords = o.split("\\s");
+        if (counter == 0) {
+            words[0] = new Word(variableWords[0]);
+            counter++;
+        }
+
+        for (int i = 0; i < variableWords.length; i++) {
+
+            for (int j = 0; words[j] != null; j++) {
+                equals = words[j].getContent().equals(variableWords[i]);
+                if (equals && counter != 1) {
+                    words[j].increaseFrequency();
+                    equalsFlag = false;
+                    break;
+                } else if (counter == 1) {
+                    equalsFlag = false;
+                    break;
+                }
+            }
+
+            if (equalsFlag) {
+                words[counter] = new Word(variableWords[i]);
                 counter++;
             }
+            equalsFlag = true;
+
         }
     }
 
+
     public String toString() {
         StringBuilder sb = new StringBuilder();
+
 
         for (int i = 0; words[i] != null; i++) {
             sb.append(words[i].toString() + "\n");
         }
 
-//        sb.deleteCharAt(sb.length() - 1);
+        sb.deleteCharAt(sb.length() - 1);
         return sb.toString();
     }
 }
