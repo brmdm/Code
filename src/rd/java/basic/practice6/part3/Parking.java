2b61e7f27da4b20a74733751829fe684f9e4a15f
brmdm <N12c345hjkokiaf>
2020-08-20 19:49:45 +0300

    Synchronization

--- src/rd/java/basic/practice6/part3/Parking.java
+++ src/rd/java/basic/practice6/part3/Parking.java
@@ -1,22 +1,47 @@
 package rd.java.basic.practice6.part3;
 
 public class Parking {
+    private int[] parking;
+    private int counter;
 
     public Parking(int capacity) {
-
+        this.parking = new int[capacity];
     }
 
     public boolean arrive(int k) {
-        return false;
+        if (k < 0 || k > (parking.length - 1)){
+            throw new IllegalArgumentException();
+        }
+        counter = parking.length;
 
+
+
+
+
+
+        return false;
     }
 
     public boolean depart(int k) {
-        return false;
+        if (k < 0 || k > (parking.length - 1)){
+            throw new IllegalArgumentException();
+        }
+        if (parking[k] == 1) {
+            parking[k] = 0;
+            print();
+            System.out.println(", true");
+            return true;
+        } else {
+            print();
+            System.out.println(", false");
+            return false;
+        }
     }
 
     public void print() {
-
+        for (int i = 0; i < parking.length; i++) {
+            System.out.print(parking[i]);
+        }
     }
 }
 
--- src/rd/java/basic/practice6/part3/Part3.java
+++ src/rd/java/basic/practice6/part3/Part3.java
@@ -1,4 +1,14 @@
 package rd.java.basic.practice6.part3;
 
 public class Part3 {
+    public static void main(String[] args) {
+        Parking parking = new Parking(4);
+        parking.arrive(2);  // 0010, true
+        parking.arrive(3);  // 0011, true
+        parking.arrive(2);  // 1011, true
+        parking.arrive(2);  // 1111, true
+        parking.arrive(2);  // 1111, false
+        parking.depart(1);  // 1011, true
+        parking.depart(1);  // 1011, false
+    }
 }
