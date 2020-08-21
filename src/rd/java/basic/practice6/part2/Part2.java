7eeefb723395725970cd75c888913a2b1f05de88
brmdm <N12c345hjkokiaf>
2020-08-21 09:11:04 +0300

    Synchronization

--- src/rd/java/basic/practice6/part2/Part2.java
+++ src/rd/java/basic/practice6/part2/Part2.java
@@ -6,39 +6,39 @@ import java.util.List;
 public class Part2 {
     public static List<Integer> arrayList = new ArrayList();
     public static List<Integer> linkedList = new ArrayList();
-    public static final int K = 4;
+    public static final int K = 3;
+    private static int counter = 0;
 
     public static void main(String[] args) {
-        fill(1000);
-        System.out.print("ArrayList#Index: " + removeByIndex(arrayList, K) + " ms\n");
+        fill(7);
+        System.out.print("ArrayList#Index: " + (System.currentTimeMillis() - removeByIndex(arrayList, K)) + " ms\n");
         System.out.print("LinkedList#Index: " + removeByIndex(linkedList, K) + " ms\n");
         System.out.print("ArrayList#Iterator: " + removeByIterator(arrayList, K) + " ms\n");
         System.out.print("LinkedList#Iterator: " + removeByIterator(linkedList, K) + " ms");
     }
 
 
-
     public static long removeByIndex(final List<Integer> list, final int k) {
-        long start = System.currentTimeMillis();
-        for (int i = 0; i < 500; i++) {
-            list.remove(i);
+        int n = list.size();
+        if (n == 1) {
+            return System.currentTimeMillis();
         }
+        list.remove(k-1);
 
-
-        return System.currentTimeMillis() - start;
+        return (removeByIndex(list, k) + k-1) % n + 1;
     }
+
 
     public static long removeByIterator(final List<Integer> list, int k) {
+        int n = list.size();
         long start = System.currentTimeMillis();
-
 
 
         return System.currentTimeMillis() - start;
     }
 
 
-
-    public static void fill (int n) {
+    public static void fill(int n) {
         for (int i = 0; i < n; i++) {
             arrayList.add(i);
             linkedList.add(i);
