611d703abcc1e32e6cc9cafb6ca510cf34eff68b
brmdm <N12c345hjkokiaf>
2020-08-22 11:17:04 +0300

    Synchronization

--- src/rd/java/basic/practice6/part2/Part2.java
+++ src/rd/java/basic/practice6/part2/Part2.java
@@ -1,38 +1,57 @@
 package rd.java.basic.practice6.part2;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Part2 {
     public static List<Integer> arrayList = new ArrayList();
-    public static List<Integer> linkedList = new ArrayList();
-    public static final int K = 3;
-    private static int counter = 0;
+    public static List<Integer> linkedList = new LinkedList<>();
+    public static final int K = 4;
+    public static final int CAPACITY = 10_000_0;
 
     public static void main(String[] args) {
-        fill(7);
-        System.out.print("ArrayList#Index: " + (System.currentTimeMillis() - removeByIndex(arrayList, K)) + " ms\n");
-        System.out.print("LinkedList#Index: " + removeByIndex(linkedList, K) + " ms\n");
-        System.out.print("ArrayList#Iterator: " + removeByIterator(arrayList, K) + " ms\n");
-        System.out.print("LinkedList#Iterator: " + removeByIterator(linkedList, K) + " ms");
+        fill(CAPACITY);
+        System.out.println("ArrayList#Index: " + removeByIndex(arrayList, K) + " ms");
+        System.out.println("LinkedList#Index: " + removeByIndex(linkedList, K) + " ms");
+        fill(CAPACITY);
+        System.out.println("ArrayList#Iterator: " + removeByIterator(arrayList, K) + " ms");
+        System.out.println("LinkedList#Iterator: " + removeByIterator(linkedList, K) + " ms");
     }
 
 
     public static long removeByIndex(final List<Integer> list, final int k) {
-        int n = list.size();
-        if (n == 1) {
-            return System.currentTimeMillis();
-        }
-        list.remove(k-1);
+        long start = System.currentTimeMillis();
 
-        return (removeByIndex(list, k) + k-1) % n + 1;
+        int technicalVariable = k - 1;
+        int i;
+
+        for (i = technicalVariable; list.size() > 1; ) {
+            list.remove(i);
+            i = ((i + technicalVariable) % list.size());
+        }
+
+        return System.currentTimeMillis() - start;
     }
 
 
     public static long removeByIterator(final List<Integer> list, int k) {
-        int n = list.size();
         long start = System.currentTimeMillis();
 
+        int i = k - 1;
+        int technicalVariable = 0;
+
+        while (list.size() > 1) {
+            Iterator<Integer> it = list.iterator();
+            while (it.hasNext()) {
+                it.next();
+                if (technicalVariable++ == i) {
+                    it.remove();
+                    i += k;
+                }
+            }
+        }
 
         return System.currentTimeMillis() - start;
     }
